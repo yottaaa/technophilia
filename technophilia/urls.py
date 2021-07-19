e1c45fa653cebd46d6617802bead01feee76a7c8
Erick Bautista <yotterick7@gmail.com>
2021-07-19 21:38:19 +0800

    Add login and logout url

--- technophilia/urls.py
+++ technophilia/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from account import views as account_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', account_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
