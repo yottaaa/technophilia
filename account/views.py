f3d6ea94f5207e58d344efd30a5cd7446ad8dbca
Erick Bautista <yotterick7@gmail.com>
2021-07-19 18:13:20 +0800

    Add messages

--- account/views.py
+++ account/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
-	form = UserCreationForm()
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		# if form is valid redirect to home url with success message
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			messages.success(request, "Welcome {}".format(username))
+			return redirect('blog-home')
+	else:
+		form = UserCreationForm()
 	context = {
 		'form':form,
-		'title': 'Sign Up'
+		'title': 'Sign Up',
 	}
 	return render(request, 'account/register.html', context)
