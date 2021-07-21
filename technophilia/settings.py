5cdc8ed514409c7a89694c9bec8253cc03b3d8de
Erick Bautista <yotterick7@gmail.com>
2021-07-21 10:09:56 +0800

    Modify Media files configuration

--- technophilia/settings.py
+++ technophilia/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 # Media uploads
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_upload')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media_upload/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
