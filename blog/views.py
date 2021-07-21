e9e2220040c71f8a40f8d94a25f34b7d2d9f5067
Erick Bautista <yotterick7@gmail.com>
2021-07-21 10:50:21 +0800

    Change Post order to descending order

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 
 def home(request):
 	context = {}
-	context['posts'] = Post.objects.all()
+	context['posts'] = Post.objects.all().order_by('-date_posted')
 	context['title'] = "Home"
 	return render(request, "blog/index.html", context)
 
