dcfc4f861947d759a3da271ce9a3b08a96d2b6bc
Erick Bautista <yotterick7@gmail.com>
2021-07-22 14:05:32 +0800

    Change template dir

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def home(request):
 	context = {}
 	context['posts'] = Post.objects.all().order_by('-date_posted')
 	context['title'] = "Home"
-	return render(request, "blog/index.html", context)
+	return render(request, "blog/home.html", context)
 
 def about(request):
 	context = {}
