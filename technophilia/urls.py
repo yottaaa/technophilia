76e33f1d5dd0fc1d265382bfdaf29d572f450e35
Erick Bautista <yotterick7@gmail.com>
2021-07-25 09:22:47 +0800

    Add update profile route

--- technophilia/urls.py
+++ technophilia/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
     path('profile/', account_views.profile, name='profile'),
+    path('profile/update/', account_views.updateProfile, name='update-profile'),
     path('', include('blog.urls')),
 ]
 
