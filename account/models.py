e1d06be734e241d754fdd51dd41d1310cfe413dc
Erick Bautista <yotterick7@gmail.com>
2021-07-25 09:12:44 +0800

    Change ImageField to ResizedImageField

--- account/models.py
+++ account/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
 # Create your models here.
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	pic = models.ImageField(default='profile_pics/default_avatar.jpg', upload_to='profile_pics')
+	pic = ResizedImageField(size=[300, 300], quality=75, upload_to='profile_pics', default='profile_pics/default_avatar.jpg', force_format='JPEG')
 
 	def __str__(self):
 		return "{} profile".format(self.user.username)
