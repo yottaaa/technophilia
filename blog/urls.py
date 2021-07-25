ade9c7a3129e83de1168c21cb0c1bc093e377cf9
Erick Bautista <yotterick7@gmail.com>
2021-07-25 09:20:39 +0800

    Add blog detail route

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="blog-home"),
+    path('detail/<str:pk>/', views.blogDetail, name="blog-detail"),
     path('about/', views.about, name="blog-about"),
 ]
