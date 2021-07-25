12e74a1d7df8daad76c0de30153afae85cffdea1
Erick Bautista <yotterick7@gmail.com>
2021-07-25 09:16:33 +0800

    Change ImageField to ResizedImageField

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_quill.fields import QuillField
 from django.utils import timezone
+from django_resized import ResizedImageField
 
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=200)
-	banner = models.ImageField(default='banner_pics/default_banner.jpg', upload_to='banner_pics')
+	banner = ResizedImageField(size=[600, 400], quality=90, upload_to='banner_pics', default='banner_pics/default_banner.jpg', force_format='JPEG')
 	caption = models.TextField(null=True,blank=True)
 	content = QuillField()
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
