e2e680a0d3899a2b4db81b246e017dba5854628a
Erick Bautista <yotterick7@gmail.com>
2021-07-25 09:15:30 +0800

    Add edit profile view

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 from blog.models import Post
 from django.contrib.auth.models import User
@@ -29,9 +29,11 @@ def register(request):
 def profile(request):
 	context = {}
 	current_logged_user = request.user
+
 	if request.GET:
 		getuser = User.objects.get(pk=request.GET['uid'])
 		posts = Post.objects.filter(author=getuser)
+
 		context['title'] = "{}'s profile".format(getuser.username)
 		context['posts'] = posts
 		context['getuser'] = getuser
@@ -39,3 +41,28 @@ def profile(request):
 		context['isEditable'] = (current_logged_user == getuser)
 
 	return render(request, 'account/profile.html', context)
+
+@login_required
+def updateProfile(request):
+	context = {}
+
+	if request.method == 'POST':
+		user_form = UserUpdateForm(request.POST, instance=request.user)
+		profile_form = ProfileUpdateForm(
+			request.POST,
+			request.FILES,
+			instance=request.user.profile)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, "Update profile successfully.")
+			return redirect('update-profile')
+	else:
+		user_form = UserUpdateForm(instance=request.user)
+		profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+	context['user_form'] = user_form
+	context['profile_form'] = profile_form
+	context['title'] = 'Update profile'
+	
+	return render(request, 'account/update.html', context)
