e8e1eb7d05a223f290c90d8e272ef0849aa12aa4
Erick Bautista <yotterick7@gmail.com>
2021-07-25 09:21:10 +0800

    Add blog detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from .forms import ContentFieldForm
 
 # Create your views here.
 
@@ -9,6 +10,16 @@ def home(request):
 	context['posts'] = Post.objects.all().order_by('-date_posted')
 	context['title'] = "Home"
 	return render(request, "blog/home.html", context)
+
+def blogDetail(request, pk):
+	context = {}
+	post = Post.objects.get(pk=pk)
+	content = ContentFieldForm(instance=post)
+	context['post'] = post
+	context['content'] = content
+	context['title'] = '{}'.format(post.title) 
+	context['isEditable'] = (post.author == request.user)
+	return render(request, "blog/detail.html", context)
 
 def about(request):
 	context = {}
