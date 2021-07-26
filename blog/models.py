331ebff63a643eee3196f1538170b3aa1f86a895
Erick Bautista <yotterick7@gmail.com>
2021-07-26 22:53:29 +0800

    Change content model to RichTextUploadingField

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_quill.fields import QuillField
 from django.utils import timezone
-from django_resized import ResizedImageField
+
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=200)
-	banner = ResizedImageField(size=[600, 400], quality=90, upload_to='banner_pics', default='banner_pics/default_banner.jpg', force_format='JPEG')
+	img_url = models.TextField(default="https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80")
 	caption = models.TextField(null=True,blank=True)
-	content = QuillField()
+	content = RichTextUploadingField(blank=True, null=True)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	date_posted = models.DateTimeField(default=timezone.now)
 
