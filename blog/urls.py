650952b3e16f517a90493bbc92c70311df4e4845
Erick Bautista <yotterick7@gmail.com>
2021-07-26 22:49:20 +0800

    Add blog create route

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="blog-home"),
     path('detail/<str:pk>/', views.blogDetail, name="blog-detail"),
+    path('create/', views.blogCreate, name="blog-create"),
     path('about/', views.about, name="blog-about"),
 ]
