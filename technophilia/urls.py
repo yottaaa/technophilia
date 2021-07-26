408463308734765e5ec3afe767c5decd373ea805
Erick Bautista <yotterick7@gmail.com>
2021-07-26 22:45:25 +0800

    Add ckeditor url

--- technophilia/urls.py
+++ technophilia/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('register/', account_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
