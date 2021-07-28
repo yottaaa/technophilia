5b3dca37912b681fc3d42c5cb8e4ab89bf467ead
Erick Bautista <yotterick7@gmail.com>
2021-07-28 18:51:12 +0800

    Add pagination and add restriction on creating blog

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,31 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-from .models import Post
+from .models import Post, BlogView
 from .forms import BlogForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def home(request):
 	context = {}
-	context['posts'] = Post.objects.all().order_by('-date_posted')
+	posts = Post.objects.all().order_by('-date_posted')
+	paginator = Paginator(posts, 5)
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+
+	context['posts'] = page_obj
 	context['title'] = "Home"
 	return render(request, "blog/home.html", context)
 
 def blogDetail(request, pk):
 	context = {}
 	post = Post.objects.get(pk=pk)
+	blog_views = BlogView.objects.all()
+
+	if not blog_views.filter(blog=post,viewer=request.user).exists():
+		BlogView.objects.create(blog=post,viewer=request.user)
+
 	context['post'] = post
 	context['title'] = '{}'.format(post.title) 
 	context['isEditable'] = (post.author == request.user)
@@ -29,14 +40,16 @@ def blogCreate(request):
 	if request.method == 'POST':
 		blog_form = BlogForm(request.POST)
 		if blog_form.is_valid():
-			blog_form.instance.author = request.user
-			blog_form.save()
-			messages.success(request, "Create new blog successfully.")
-			return redirect('blog-home')
-	else:
-		blog_form = BlogForm()
-		context['blog_form'] = blog_form
-
+			if not Post.objects.filter(title=blog_form.instance.title).exists():
+				blog_form.instance.author = request.user
+				blog_form.save()
+				messages.success(request, "Create new blog successfully.")
+				return redirect('blog-home')
+			else:
+				messages.warning(request, "Title \"{}\" are existed.".format(blog_form.instance.title))
+			
+	blog_form = BlogForm()
+	context['blog_form'] = blog_form
 	context['title'] = "Add new blog"
 	return render(request, "blog/create.html", context)
 
