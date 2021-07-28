bcb0bfdb6528fd964563da993fef3afa20c8e8f9
Erick Bautista <yotterick7@gmail.com>
2021-07-28 18:46:11 +0800

    Add context processor

---
+++ blog/context_processors.py
@@ -0,0 +1,23 @@
+from .models import Post, BlogView
+
+def recommendationContext(request):
+	context = {}
+	blog_views = BlogView.objects.values('blog').distinct()
+	posts = list()
+
+	# Make post datastructure
+	for blog in blog_views:
+		post = Post.objects.get(pk=blog.get('blog'))
+		views = BlogView.objects.filter(blog=post).count()
+		posts.append({
+			'blog':post,
+			'views':views,
+		})
+
+	
+	posts = sorted(posts, key=lambda e: e['views'], reverse=True)
+
+	print(posts)
+
+	context['recommended_posts'] = posts
+	return context
