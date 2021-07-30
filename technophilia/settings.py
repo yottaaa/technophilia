39e2adb426fd48d92376640f7551f6f036019348
Erick Bautista <yotterick7@gmail.com>
2021-07-30 21:13:53 +0800

    Add reset password

--- technophilia/settings.py
+++ technophilia/settings.py
@@ -167,3 +167,15 @@ CKEDITOR_CONFIGS = {
         'codeSnippet_theme': 'monokai_sublime',
     },
 }
+
+
+# EMAIL SERVICES
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER= 'yotterick7@gmail.com'
+EMAIL_HOST_PASSWORD='qxytsspkhhqbqtvm'
+
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
