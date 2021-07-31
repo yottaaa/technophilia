26eb96ccba5e69476df772f55c67c1d41de07ed0
Erick Bautista <yotterick7@gmail.com>
2021-07-31 11:21:54 +0800

    Add reset password config

--- technophilia/settings.py
+++ technophilia/settings.py
@@ -174,8 +174,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER= 'yotterick7@gmail.com'
-EMAIL_HOST_PASSWORD='qxytsspkhhqbqtvm'
-
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
